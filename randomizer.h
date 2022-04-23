e182d1c20ce03f6f3fde90426fc742f003c9138c
Garrick Chan <changarrickt@gmail.com>
2022-04-22 17:32:59 -0700

    first commit

---
+++ randomizer.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+// call this if you want to force your program to use
+// a different random number sequence every time you run it:
+void
+TimeOfDaySeed( )
+{
+	struct tm y2k = { 0 };
+	y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
+	y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
+
+	time_t  timer;
+	time( &timer );
+	double seconds = difftime( timer, mktime(&y2k) );
+	unsigned int seed = (unsigned int)( 1000.*seconds );    // milliseconds
+	srand( seed );
+}
